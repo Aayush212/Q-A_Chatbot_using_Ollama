fe0edbbbdfd2097ac64c6171b677ae6a5eff272c
Aayush Mishra <89451987+Aayush212@users.noreply.github.com>
2025-01-30 14:30:28 +0530

    Add files via upload

---
+++ Ollama-Chatbot/app.py
@@ -0,0 +1,47 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+import streamlit as st 
+
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+#Langsmith Tracking
+os.environ["LANGCHAIN_API_KEY"]="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"
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_PROJECT"]="Simple Q&A Chatbot with Ollama"
+
+#Prompt Template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please respond to user queries"),
+        ("user","Question:{question}")
+    ]
+)
+
+def generate_response(question,engine,temperature,max_tokens):
+    
+    llm=Ollama(model=engine)
+    output_parser=StrOutputParser()
+    chain=prompt|llm|output_parser
+    answer=chain.invoke({'question': question})
+    return answer
+
+#Select the Ollama model
+engine=st.sidebar.selectbox("Select Ollama model",["mistral","gemma2"])
+
+#Adjust response parameter
+temperature=st.sidebar.slider("Temperature",min_value=0.0,max_value=1.0,value=0.7)
+max_tokens=st.sidebar.slider("Max Tokens",min_value=50,max_value=300,value=150)
+
+#Main Interface for user input
+st.write("Go ahead and ask any questions")
+user_input=st.text_input("You:")
+
+if user_input:
+    response=generate_response(user_input,engine,temperature,max_tokens)
+    st.write(response)
+else:
+    st.write("Please provide the user input")
