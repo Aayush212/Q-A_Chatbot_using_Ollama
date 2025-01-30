c5da03037196b7b951f44013b3678d6eb152759a
Aayush Mishra <89451987+Aayush212@users.noreply.github.com>
2025-01-30 14:31:33 +0530

    Update app.py

--- Ollama-Chatbot/app.py
+++ Ollama-Chatbot/app.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #Langsmith Tracking
-os.environ["LANGCHAIN_API_KEY"]="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"
+os.environ["LANGCHAIN_API_KEY"]="Your_Langchain_API_Key"
 os.environ["LANGCHAIN_TRACING_V2"]="true"
 os.environ["LANGCHAIN_PROJECT"]="Simple Q&A Chatbot with Ollama"
 
